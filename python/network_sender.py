a4aa49d96cf61881a597f198a5e667f392ef6bdd
Zoryanchik <zoryan2006@gmail.com>
2026-01-21 02:12:20 +0200

    Started network Sender using udp connection, and made send_face_data function

---
+++ python/network_sender.py
@@ -0,0 +1,43 @@
+from pythonosc import udp_client
+from pythonosc.osc_message_builder import OscMessageBuilder
+import time
+from config import Config
+
+class NetworkSender:
+    def __init__(self):
+        print("Initializing Network Sender...")
+        try:
+            self.client = udp_client.SimpleUDPClient(Config.UnityIp, Config.UnityPort)
+            self.connected = True # Just a flag
+            print("Network sender is ready")
+        except Exception as e:
+            print(f"Failed to initialize Network Sender: {e}")
+            self.connected = False
+        
+        self.packet_sent = 0
+        self.last_send_time = 0
+        self.send_interval = 1 / 60  # Send at 60 packets per s
+   
+    def send_face_data(self,data):
+        if not self.connected:
+            return
+
+        current_time = time.time()
+        if current_time - self.last_send_time < self.send_interval:
+            return 
+
+        try:
+            # here we send the face data
+            self.client.send_message("/face/mouth", float(data['mouth_open']))
+
+            self.client.send_message("/face/eye_left", float(data['eye_left']))
+            self.client.send_message("/face/eye_right", float(data['eye_right']))
+
+            self.client.send_message("/face/head_x", float(data['head_x']))
+            self.client.send_message("/face/head_y", float(data['head_y']))
+
+            self.packet_sent += 1
+            self.last_send_time = current_time
+        except Exception as e:
+            print(f"Error sending face data: {e}")
+            self.connected = False
